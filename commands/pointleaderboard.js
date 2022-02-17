cbb0c345a9464bc2abedf8d219d9c5b1d74409c3
FiveCoolCool <99565830+FiveCoolCool@users.noreply.github.com>
2022-02-16 21:14:11 -0600

    Create pointleaderboard.js

---
+++ commands/pointleaderboard.js
@@ -0,0 +1,45 @@
+const { MessageEmbed } = require("discord.js");
+const userSchema = require('../schemas/userSchema')
+
+module.exports.run = async (client, message, args) => {
+
+  const data = await userSchema.find().sort({ points: -1 });
+
+  if (data.length < 1) return message.channel.send("No leaderboard");
+  let myrank = data.map(data => data.userId).indexOf(message.author.id) + 1 || "N/A";
+
+  data.length = 5;
+  let lb = [];
+  for (let i in data) {
+    let id = data[i].userId;
+    let user = data[i].userName;
+    // let user = await client.users.fetch(id);
+    // user = user ? user.tag : "Unknown User#0000";
+    let rank = data.indexOf(data[i]) + 1;
+    let points;
+    if (data[i].points > 0) {
+      points = data[i].points;
+    } else {
+      continue
+    }
+    lb.push({
+      user: { id, tag: user },
+      rank,
+      points,
+    });
+  };
+  const embed = new MessageEmbed()
+    .setTitle("Leaderboard")
+    .setDescription("Users with the most points!")
+    .setColor("RANDOM")
+  lb.forEach(d => {
+    embed.addField(`${d.rank}. ${d.user.tag}`, `**Points** - ${d.points}`);
+  });
+  embed.setFooter(`Your Position: ${myrank}`);
+  return message.channel.send({ embeds: [embed] });
+};
+
+module.exports.help = {
+  name: "pointleaderboard",
+  aliases: ['plb']
+};
